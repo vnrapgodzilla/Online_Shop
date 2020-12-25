fb1f4a3962cea7fc5ea3bda3214379e0ce242d7f
vnrapgodzilla <dotaallstars1996@gmail.com>
2020-12-25 22:46:35 +0700

    Online_Shop Common

---
+++ Common/CommonConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Common
+{
+    public static class CommonConstants
+    {
+        public static string MEMBER_GROUP = "MEMBER";
+        public static string ADMIN_GROUP = "ADMIN";
+        public static string MOD_GROUP = "MOD";
+    }
+}
---
+++ Common/StringHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Common
+{
+    public static class StringHelper
+    {
+        public static string ConvertToUnSign(string input)
+        {
+            if (!String.IsNullOrEmpty(input))
+            {
+                input = input.Trim();
+                for (int i = 0x20; i < 0x30; i++)
+                {
+                    input = input.Replace(((char)i).ToString(), " ");
+                }
+                input = input.Replace(".", "-");
+                input = input.Replace(" ", "-");
+                input = input.Replace(",", "-");
+                input = input.Replace(";", "-");
+                input = input.Replace(":", "-");
+                input = input.Replace("  ", "-");
+                Regex regex = new Regex(@"\p{IsCombiningDiacriticalMarks}+");
+                string str = input.Normalize(NormalizationForm.FormD);
+                string str2 = regex.Replace(str, string.Empty).Replace('đ', 'd').Replace('Đ', 'D');
+                while (str2.IndexOf("?") >= 0)
+                {
+                    str2 = str2.Remove(str2.IndexOf("?"), 1);
+                }
+                while (str2.Contains("--"))
+                {
+                    str2 = str2.Replace("--", "-").ToLower();
+                }
+                return str2;
+            }
+            return "";
+        }
+    }
+}
